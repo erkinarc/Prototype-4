7e1151c0d32e8c858c65e7c37b3d35a25f0c3684
Erkin Arıcı <goktanarici@gmail.com>
2025-06-02 14:03:05 +0300

    added powerup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public bool hasPowerup = false;
+    private float powerupStrength = 15.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,5 +22,38 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * moveSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        Debug.Log("Powerup has ended");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            Debug.Log("Player collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            
+        }
+        else if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player collided with " + collision.gameObject.name + " without powerup");
+        }
     }
 }
